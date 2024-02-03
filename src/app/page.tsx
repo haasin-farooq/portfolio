4f476966999e399ea691f4c863c6f4f46b41ba76
Haasin Farooq <haasin.farooq@gmail.com>
2024-02-04 00:43:46 +0200

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-
-import ProfilePicture from "../assets/images/profile-picture.jpg";
 import { ProfileSummary } from "@/components/home/ProfileSummary";
 
 const HomePage = () => {
